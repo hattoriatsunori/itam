f7a67748f979b552a9507d08093a474253f1d259
Atsunori Hattori <tihove7k@gmail.com>
2023-04-20 08:08:34 +0900

    echo setup

---
+++ server.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Device struct {
+	gorm.Model
+	SerialNumber string `json:"serial_number"`
+	Notation     string `json:"notation"`
+}
+
+var Db *gorm.DB
+var err error
+
+func init() {
+	dsn := "root:mysql@tcp(127.0.0.1:3306)/itam?charset=utf8mb4&parseTime=True&loc=Local"
+	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	Db.AutoMigrate(&Device{})
+}
+
+func main() {
+
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.GET("/devices", getDevices)
+	e.GET("/devices/:id", getDevice)
+	e.POST("/devices", addDevice)
+	e.PUT("/devices/:id", updateDevice)
+	e.DELETE("/devices/:id", deleteDevice)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func getDevices(c echo.Context) error {
+	var devices []Device
+	Db.Find(&devices)
+	return c.JSON(http.StatusOK, devices)
+}
+func getDevice(c echo.Context) error {
+	var device Device
+	id := c.Param("id")
+	idint, err := strconv.Atoi(id)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "id is not integer")
+	}
+
+	Db.First(&device, idint)
+	return c.JSON(http.StatusOK, device)
+}
+func addDevice(c echo.Context) error {
+	device := new(Device)
+	if err := c.Bind(device); err != nil {
+		return err
+	}
+	Db.Create(&device)
+	return c.JSON(http.StatusOK, device)
+}
+func updateDevice(c echo.Context) error {
+	return c.String(http.StatusOK, "updateDevice")
+}
+func deleteDevice(c echo.Context) error {
+	return c.String(http.StatusOK, "deleteDevice")
+}
